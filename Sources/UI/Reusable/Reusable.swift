5cc68fdbcf26c80661b28252d664cf9678b4503d
Yue Cai <yuecai.nju@gmail.com>
2021-12-03 00:44:29 +0800

    add UI

---
+++ Sources/UI/Reusable/Reusable.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Ref: https://github.com/AliSoftware/Reusable
+
+public protocol Reusable: class {
+  /// The reuse identifier to use when registering and later dequeuing a reusable cell
+  static var reuseIdentifier: String { get }
+}
+
+// MARK: - Default implementation
+
+public extension Reusable {
+  /// By default, use the name of the class as String for its reuseIdentifier
+  static var reuseIdentifier: String {
+    return String(describing: self)
+  }
+}
+
